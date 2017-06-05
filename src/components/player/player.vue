1a5402c9bd04b43f96a75b8badb0dafb6c0e24bc
lz-lee <296452017@qq.com>
2017-06-06 00:05:22 +0800

    动画钩子enter／leave 都是动画初始状态，须要执行done函数，afterenter／afterleave钩子里需要移除动画样式

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,19 +1,145 @@
 <template>
     <div class="player" v-show="playlist.length > 0">
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave">
         <div class="normal-player" v-show="fullscreen">
-            播放器
+            <div class="background">
+                <img width="100%" height="100%" :src="currentSong.image">
+            </div>
+            <div class="top">
+                <div class="back" @click="back">
+                    <i class="icon-back"></i>
+                </div>
+                <h1 class="title" v-html="currentSong.name"></h1>
+                <h2 class="subtitle" v-html="currentSong.singer"></h2>
+            </div>
+            <div class="middle">
+                <div class="middle-l">
+                    <div class="cd-wrapper" ref="cdWrapper">
+                        <div class="cd">
+                            <img class="image" :src="currentSong.image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="operators">
+                    <div class="icon i-left">
+                        <i class="icon-sequence"></i>
+                    </div>
+                    <div class="icon i-left">
+                        <i class="icon-prev"></i>
+                    </div>
+                    <div class="icon i-center">
+                        <i class="icon-play"></i>
+                    </div>
+                    <div class="icon i-right">
+                        <i class="icon-next"></i>
+                    </div>
+                    <div class="icon i-right">
+                        <i class="icon icon-not-favorite"></i>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="min-player" v-show="!fullscreen"></div>
+    </transition>
+    <transition name="mini">
+        <div class="min-player" v-show="!fullscreen" @click="open">
+            <div class="icon">
+                <img width="40" height="40" :src="currentSong.image">
+            </div>
+            <div class="text">
+                <h2 class="name" v-html="currentSong.name"></h2>
+                <p class="desc" v-html="currentSong.singer"></p>
+            </div>
+            <div class="control"></div>
+            <div class="control">
+                <i class="icon-playlist"></i>
+            </div>
+        </div>
+    </transition>
     </div>
 </template>
 <script>
-    import {mapGetters} from 'vuex'
+    import {mapGetters, mapMutations} from 'vuex'
+    import animations from 'create-keyframe-animation'
+    import {prefix} from 'common/js/prefix'
+
+    const transform = prefix('transform')
+
     export default {
         computed: {
             ...mapGetters([
                 'fullscreen',
-                'playlist'
+                'playlist',
+                'currentSong'
                 ])
+        },
+        methods: {
+            back() {
+                this.setFullScreen(false)
+            },
+            open() {
+                this.setFullScreen(true)
+            },
+            enter(el, done) {
+                const {x, y, scale} = this._getPosandScale()
+
+                let animation = {
+                    0: {
+                        transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+                    },
+                    60: {
+                        transform: `translate3d(0, 0, 0) scale(1.1)`
+                    },
+                    100: {
+                        transform: `translate3d(0, 0, 0) scale(1)`
+                    }
+                }
+                animations.registerAnimation({
+                    name: 'move',
+                    animation,
+                    presets: {
+                        duration: 500,
+                        easing: 'linear'
+                    }
+                })
+
+                animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+            },
+            afterEnter() {
+                // 动画执行完后，清空动画，移除动画样式
+                animations.unregisterAnimation('move')
+                this.$refs.cdWrapper.style.animation = ''
+            },
+            leave(el, done) {
+                this.$refs.cdWrapper.style.transition = 'all .4s'
+                const {x, y, scale} = this._getPosandScale()
+                this.$refs.cdWrapper.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+                this.$refs.cdWrapper.addEventListener('transitionend', done)
+            },
+            afterLeave() {
+                this.$refs.cdWrapper.style.transition = ''
+                this.$refs.cdWrapper.style[transform] = ''
+            },
+            _getPosandScale() {
+                const targetWidth = 40
+                const paddingTop = 80
+                const paddingBottom = 30
+                const paddingLeft = 40
+                const width = window.innerWidth * 0.8
+                const scale = targetWidth / width
+                // 起始位置 相对于 终止位置的坐标，x为负值，y为正值
+                const x = -(window.innerWidth / 2 - paddingLeft)
+                const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+                return {x, y, scale}
+            },
+            ...mapMutations({
+                setFullScreen: 'SET_FULL_SCREEN'
+            })
         }
     }
 </script>
@@ -29,6 +155,110 @@
             bottom 0
             z-index 4
             background $color-background
+            &.normal-enter-active,&.normal-leave-active
+                transition all .4s
+                .top, .bottom
+                    transition all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+            // 起始状态
+            &.normal-enter,&.normal-leave-to
+                opacity 0
+                .top
+                    transform translate3d(0, -100px, 0)
+                .bottom
+                    transform translate3d(0, 100px, 0)
+            .background
+                position absolute
+                left 0
+                top 0
+                width 100%
+                height 100%
+                z-index -1
+                filter blur(20px)
+            .top
+                position relative
+                margin-bottom 25px
+                .back
+                    position absolute
+                    top 0
+                    left 6px
+                    z-index 5
+                    .icon-back
+                        display block
+                        padding 10px
+                        font-size $font-size-large-x
+                        color $color-theme
+                        transform rotate(-90deg)
+                .title
+                    width 70%
+                    margin 0 auto
+                    line-height 40px
+                    text-align center
+                    no-wrap()
+                    font-size $font-size-large
+                    color $color-text
+                .subtitle
+                    line-height 20px
+                    text-align center
+                    font-size $font-size-medium
+                    color $color-text
+            .middle
+                position: fixed
+                width: 100%
+                top: 80px
+                bottom: 170px
+                white-space: nowrap
+                font-size: 0
+                .middle-l
+                    display: inline-block
+                    vertical-align: top
+                    position: relative
+                    width: 100%
+                    height: 0
+                    padding-top: 80%
+                    .cd-wrapper
+                        position: absolute
+                        left: 10%
+                        top: 0
+                        width: 80%
+                        height: 100%
+                        .cd
+                            width: 100%
+                            height: 100%
+                            box-sizing: border-box
+                            border: 10px solid rgba(255, 255, 255, 0.1)
+                            border-radius: 50%
+                            .image
+                                position: absolute
+                                left: 0
+                                top: 0
+                                width: 100%
+                                height: 100%
+                                border-radius: 50%
+            .bottom
+                position: absolute
+                bottom: 50px
+                width: 100%
+                .operators
+                    display: flex
+                    align-items: center
+                    .icon
+                        flex: 1
+                        color: $color-theme
+                        &.disable
+                            color: $color-theme-d
+                        i
+                            font-size: 30px
+                    .i-left
+                        text-align: right
+                    .i-center
+                        padding: 0 20px
+                        text-align: center
+                        i
+                            font-size: 40px
+                    .i-right
+                        text-align: left
+                    .icon-favorite
+                        color: $color-sub-theme
         .min-player
             display flex
             align-items center
@@ -39,4 +269,34 @@
             width 100%
             height 60px
             background: $color-highlight-background
+            &.mini-enter-active,&.mini-leave-active
+                transition all .4s ease
+            &.mini-enter,&.mini-leave-to
+                opacity 0
+            .icon
+                flex 0 0 40px
+                width 40px
+                padding 0 10px 0 20px
+                img
+                    border-radius 50%
+            .text
+                display flex
+                flex-direciton columm
+                justify-content center
+                flex 1
+                line-height 20px
+                overflow hidden
+                .name
+                    margin-bottom 2px
+                    np-wrap()
+                    font-size $font-size-medium
+                    color $color-text
+                .desc
+                    np-wrap()
+                    font-size $font-size-small
+                    color $color-text-d
+            .control
+                flex 0 0 30px
+                width 30px
+                padding 0 10px
 </style>
