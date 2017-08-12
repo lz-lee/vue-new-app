3b0a444433bbc0e7311a22ea5b910fe274db5276
lz-lee <296452017@qq.com>
2017-08-12 19:21:39 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,57 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import rank from 'components/rank/rank'
-import search from 'components/search/search'
-import singer from 'components/singer/singer'
-import recommend from 'components/recommend/recommend'
-import singerDetail from 'components/singer-detail/singer-detail'
-import disc from 'components/disc/disc'
-import topList from 'components/top-list/top-list'
-import userCenter from 'components/user-center/user-center'
+// import rank from 'components/rank/rank'
+// import search from 'components/search/search'
+// import singer from 'components/singer/singer'
+// import recommend from 'components/recommend/recommend'
+// import singerDetail from 'components/singer-detail/singer-detail'
+// import disc from 'components/disc/disc'
+// import topList from 'components/top-list/top-list'
+// import userCenter from 'components/user-center/user-center'
 
 Vue.use(Router)
+
+const recommend = (resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+}
+const rank = (resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module)
+  })
+}
+const search = (resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+}
+const singer = (resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+}
+const singerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((module) => {
+    resolve(module)
+  })
+}
+const disc = (resolve) => {
+  import('components/disc/disc').then((module) => {
+    resolve(module)
+  })
+}
+const topList = (resolve) => {
+  import('components/top-list/top-list').then((module) => {
+    resolve(module)
+  })
+}
+const userCenter = (resolve) => {
+  import('components/user-center/user-center').then((module) => {
+    resolve(module)
+  })
+}
 
 export default new Router({
   routes: [
